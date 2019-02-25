d3274d5340a0ca194b0e45391b7d48e7f94d9de8
Lemburse <40856911+Lemburse@users.noreply.github.com>
2019-02-25 10:48:18 +0800

    Update predeal.py

--- predeal.py
+++ predeal.py
@@ -13,32 +13,56 @@ class predeal:
         rows, cols = img.shape[:2]
 
         img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        retval, img_at_fixed = cv.threshold(img_gray, 150, 255, cv.THRESH_BINARY)
+        retval, img_at_fixed = cv.threshold(img_gray, 200, 255, cv.THRESH_BINARY)
         newpath = fname + '-bw.' + fjpg
-        cv.imwrite(newpath, img_at_fixed)
+        cv.imwrite(newpath, img_gray)
 
     def addNoise(self,path,type):
         fname, fjpg = path.split('.', 1)
 
         im = skimage.io.imread(path)
-        dst = skimage.util.random_noise(im, mode=type, seed=None, clip=True)
+        dst = skimage.util.random_noise(im, mode=type, seed=None, clip=True) #‘gaussian’,‘localvar’,‘poisson’,‘salt’,‘pepper’,‘s&p’ ,‘speckle’
         # cv.imshow('1',dst)
         # cv.waitKey(0)
         newpath = fname + '-n.' + fjpg
         skimage.io.imsave(newpath, dst)
 
-    def rotate(self,path,degree):
+    def SaltAndPepper(self,path, percetage):
+        fname, fjpg = path.split('.', 1)
+        src = cv.imread(path)
+        NoiseImg = src
+        NoiseNum = int(percetage * src.shape[0] * src.shape[1])
+        for i in range(NoiseNum):
+            randX = random.randint(0, src.shape[0] - 1)
+            randY = random.randint(0, src.shape[1] - 1)
+            if random.randint(0, 1) == 0:
+                NoiseImg[randX, randY] = 0
+            else:
+                NoiseImg[randX, randY] = 255
+        newpath = fname + '-n.' + fjpg
+        cv.imwrite(newpath,NoiseImg)
+
+
+    def rotate(self,path,degree,flag):
+        #print(degree)
         fname, fjpg = path.split('.', 1)
         img = cv.imread(path)
         rows, cols = img.shape[:2]
 
-        if degree is 90:
+        if degree == 90:
             dst = np.rot90(img)
-            newpath = fname + '-r.' + fjpg
+            newpath = fname + flag + fjpg
             cv.imwrite(newpath, dst)
+
+        elif degree == 270:
+            #print(1)
+            dst = np.rot90(img,3)
+            newpath = fname + flag + fjpg
+            cv.imwrite(newpath, dst)
+
         else:
             # print(degree)
-
+            #print(2)
             degree_new = math.degrees(math.atan(rows / cols))
             # print(degree_new)
             length = math.pow(rows * rows + cols * cols, 1 / 2)
@@ -67,5 +91,5 @@ class predeal:
 
             M = cv.getRotationMatrix2D((cols / 2, rows / 2), degree, propotion)
             dst = cv.warpAffine(img, M, (cols, rows))
-            newpath = fname + '-r.' + fjpg
+            newpath = fname + flag + fjpg
             cv.imwrite(newpath, dst)
